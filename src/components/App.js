c553dcbd6a1f312d42856f6c4d3f4ae1ad5453d3
AlenaNik <alena.nik.digital@gmail.com>
2020-04-14 13:08:24 +0200

    comliting errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import AuthenticatedApp from './AuthenticatedApp'
 import UnauthenticatedApp from './UnauthenticatedApp'
 import { UserProvider } from "../context/AuthContext";
--- src/components/UnauthenticatedApp.js
+++ src/components/UnauthenticatedApp.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, {useContext } from 'react'
 import '../components-styling/UnauthenticatedApp.scss'
 import AuthContext from '../context/AuthContext'
 import Button from './Button'
